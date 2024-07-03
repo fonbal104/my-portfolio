b7728a0f1b8be5e9c4132737e9052839412c9740
Fonacier Jr. Balatero <55889729+fonbal104@users.noreply.github.com>
2024-07-03 17:40:30 +0800

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your Portfolio Site 'Fonacier B Jr.'"; //letter subject
+	$to = 'fonacier.gowfb@gmail.com'; // Enter Your E-mail
+	$from = 'fonacier.gowfb@gmail.com'; // Admin e-mail
+	$fromName = 'Fonacier Portfolio Site'; // Your company name
+	$charset = 'UTF-8';
+?>
